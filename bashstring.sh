9bd7b4509e0cdc0940cb1f682b5a9bd4937be80e
nikhil.vargude <nikhilvargude@gmail.com>
2024-11-27 18:04:55 +0530

    added some files

---
+++ bashstring.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+#check the two string are equal or not
+
+string1="nitin"
+string2="nitin"
+
+if [ $string1 = $string2 ];
+then
+echo "Both the strings are equal"
+else
+echo "Both string are not equal"
+fi
